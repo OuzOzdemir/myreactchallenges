717f2d7fd5154df959be14ca1cf4458d93db3bde
Oguzhan Ozdemir <ouzozdev@gmail.com>
2023-02-22 14:02:08 +0300

    first commit

--- index.js
+++ index.js
@@ -435,15 +435,14 @@ ReactDOM.render(Header(), document.getElementById("root"))
 
 /*
 
-import React from "react"
-import ReactDOM from "react-dom"
-
 //Challenge: 
-
 //- Move the `footer` into its own component called "Footer" 
   and render that component inside the Page component.
 //- Move the `h1` and `ol` together into another component
   called "MainContent" and render inside Page as well.
+
+import React from "react"
+import ReactDOM from "react-dom"
 
 function Header() {
     return (
@@ -454,25 +453,40 @@ function Header() {
         </header>
     )
 }
+function Footer(){
+    return(
+        <footer>
+         <small>© 2021 Ziroll development. All rights reserved.</small>
+        </footer>
+    )
+}
+
+function Content() {    
+    return(
+        <>
+            <h1>Reasons I'm excited to learn React</h1>
+                <ol>
+                    <li>It's a popular library, so I'll be 
+                    able to fit in with the cool kids!</li>
+                    <li>I'm more likely to get a job as a developer
+                    if I know React</li>
+                </ol>
+        </>
+        )
+}
 
 function Page() {
     return (
         <div>
-            <h1>Reasons I'm excited to learn React</h1>
-            <ol>
-                <li>It's a popular library, so I'll be 
-                able to fit in with the cool kids!</li>
-                <li>I'm more likely to get a job as a developer
-                if I know React</li>
-            </ol>
-            <footer>
-                <small>© 2021 Ziroll development. All rights reserved.</small>
-            </footer>
+            <Header />
+            <Content />
+            <Footer />
         </div>
     )
 }
 
 ReactDOM.render(<Page />, document.getElementById("root"))
+
 
 */
 
